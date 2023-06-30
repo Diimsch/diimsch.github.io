7f4cca11ef053ff8188f1bdd1d186a3eaf1472a2
Diimsch <Th.Schmidt97@outlook.de>
2023-06-30 19:50:16 +0200

    project cards and images

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,12 +3,13 @@ import React from 'react';
 export type CardProps = {
   from?: Date;
   to?: Date;
-  image?: String;
-  title?: String;
-  location?: String;
-  description?: String;
-  tags?: String[];
-  additionalLinks?: String[];
+  image?: string;
+  title?: string;
+  location?: string;
+  description?: string;
+  tags?: string[];
+  href?: string;
+  additionalLinks?: string[];
 };
 
 function Card(props?: CardProps) {
@@ -37,36 +38,47 @@ function Card(props?: CardProps) {
           className='z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide  sm:col-span-2'
           aria-label='2018 to Present'
         >
-          {`${formatDate(props?.from)} - ${formatDate(props?.to)}`}
+          {props?.image ? (
+            <img
+              className='max-h-48 rounded border-2 border-slate-200/10 transition group-hover:border-slate-200/30 sm:order-1 sm:col-span-2 sm:translate-y-1'
+              src={props.image}
+            ></img>
+          ) : (
+            `${formatDate(props?.from)} - ${formatDate(props?.to)}`
+          )}
         </header>
         <div className='z-10 sm:col-span-6'>
           <h3 className='font-medium leading-snug '>
             <div>
               <a
-                className='inline-flex items-baseline font-medium leading-tight    group/link text-base'
-                href='https://upstatement.com'
+                className='inline-flex items-baseline font-medium leading-tight group/link text-base'
+                href={props?.href}
                 target='_blank'
                 rel='noreferrer'
                 aria-label='Lead Engineer at Upstatement'
               >
                 <span className='absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block'></span>
                 <span className='font-bold'>
-                  {props?.title} {' - '}
+                  {props?.title} {props?.location ? ' - ' : ''}
                   <span className='inline-block'>
                     {props?.location}
-                    <svg
-                      xmlns='http://www.w3.org/2000/svg'
-                      viewBox='0 0 20 20'
-                      fill='currentColor'
-                      className='inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px'
-                      aria-hidden='true'
-                    >
-                      <path
-                        fillRule='evenodd'
-                        d='M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z'
-                        clipRule='evenodd'
-                      ></path>
-                    </svg>
+                    {props?.href ? (
+                      <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        viewBox='0 0 20 20'
+                        fill='currentColor'
+                        className='inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px'
+                        aria-hidden='true'
+                      >
+                        <path
+                          fillRule='evenodd'
+                          d='M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z'
+                          clipRule='evenodd'
+                        ></path>
+                      </svg>
+                    ) : (
+                      <></>
+                    )}
                   </span>
                 </span>
               </a>
